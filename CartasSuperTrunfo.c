d643e51d735663db93300fd7f5b115bc03e114c4
Raphael Marques <112436208+raphntc@users.noreply.github.com>
2025-05-19 13:28:46 +0000

    Entrada e Saida

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,45 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    int ddd;
+    char nome[20];
+    int populacao;
+    float area;
+    float pib;
+    int turismo;
     
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
+    printf("Digite o código da cidade: ");
+    scanf("%i", &ddd);
+    printf("Digite o nome da cidade: ");
+    scanf("%s", &nome);
+    printf("Digite a população da cidade: ");
+    scanf("%i", &populacao);
+    printf("Digite a área da cidade: ");
+    scanf("%f", &area);
+    printf("Digite o PIB da cidade: ");
+    scanf("%i", &pib);
+    printf("Digite a quantidade de pontos turisticos da cidade: ");
+    scanf("%d", &turismo);
+
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+    printf("--- Jogo SUPER TRUNFO --- \n");
+    printf("Dados da cidade\n");
+    printf("Código da cidade:%i \n", ddd);
+    printf("Nome:%s \n", nome);
+    printf("População:%i \n", populacao);
+    printf("Área:%f \n", area);
+    printf("PIB:%i \n", pib);
+    printf("Número de pontos turísticos:%d \n", turismo);
+
+
 
     return 0;
 }
